2afa3f3cb48d84699d7bc0d4c42280970c2edda5
irving <irvingzhang0512@gmail.com>
2020-05-28 21:05:25 +0800

    [MODIFY] update downloading deeplabv3 script.

--- scripts/download_deeplab_v3_ckpt.sh
+++ scripts/download_deeplab_v3_ckpt.sh
@@ -1,10 +1,10 @@
 wget http://download.tensorflow.org/models/deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz
 wget http://download.tensorflow.org/models/deeplabv3_pascal_train_aug_2018_01_04.tar.gz
 
-mkdir -p ./data/deeplab_v3/mobile_net_model
-mkdir -p ./data/deeplab_v3/xception_model
-tar xvzf deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz -C ./data/deeplab_v3/mobile_net_model --strip=1
-tar xvzf deeplabv3_pascal_train_aug_2018_01_04.tar.gz -C ./data/deeplab_v3/xception_model --strip=1
+mkdir -p ./data/deeplab_v3/mobilenet
+mkdir -p ./data/deeplab_v3/xception
+tar xvzf deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz -C ./data/deeplab_v3/mobilenet --strip=1
+tar xvzf deeplabv3_pascal_train_aug_2018_01_04.tar.gz -C ./data/deeplab_v3/xception --strip=1
 
 rm deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz
 rm deeplabv3_pascal_train_aug_2018_01_04.tar.gz
